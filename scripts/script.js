27cbb4ac49b2eeb6a6034d470e7130f7e86c4abc
Prince Ibrahim <61180254+ibrahimaroosi@users.noreply.github.com>
2021-06-04 12:57:53 +0530

    fresh upload

---
+++ scripts/script.js
@@ -0,0 +1,29 @@
+// var x = 0;
+// slides();
+
+//   function slides() {
+//   var i = 0;
+//   var y = document.getElementsByClassName("mobile-images");
+//   for (i = 0; i < y.length; i++) {
+//     y[i].style.display = "none";
+//   }
+//   y[x].style.display = "block";
+//   if (x >= y.length - 1) {
+//     x = 0;
+//   } else {
+//     x++;
+//   }
+//   setTimeout(slides, 2000);
+// }
+
+// Uncomment the above code to enable js animation
+
+// JS sample code for dynamic start ratings
+
+//    var x = document.querySelector(':root');
+    //    rating();
+    //    function rating() {
+    //     x.style.setProperty('--rating', 4);
+    //    }
+    // The above code is example of how to change ratings dynamically
+    // We selected :root element and changed --rating variable's value
